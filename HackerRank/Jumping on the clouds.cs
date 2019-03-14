21ee0f5bc4aa43cd55fb8e3338c36f9ba0e7b2d1
arturolc <arturolc_97@hotmail.com>
2019-03-14 15:50:09 -0600

    solved

--- HackerRank/Jumping on the clouds.cs
+++ HackerRank/Jumping on the clouds.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace HackerRank
@@ -7,15 +8,45 @@ namespace HackerRank
     {
         static int jumpingOnClouds(int[] c)
         {
+            int jumps = 0;
+            int i = 0;
 
-
+            while (i < c.Length - 1)
+            {
+                if (c[i] == 0)
+                {
+                    if (i < c.Length - 2)
+                    {
+                        if (c[i + 2] == 0)
+                        {
+                            jumps++;
+                            i += 2;
+                        }
+                        else if (c[i + 1] == 0)
+                        {
+                            jumps++;
+                            i++;
+                        }
+                    }
+                    else if (c[i + 1] == 0)
+                    {
+                        jumps++;
+                        i++;
+                    }
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return jumps;
         }
+
 
         static void Main(string[] args)
         {
-            int[] a = { 7, 0, 0, 1, 0, 0, 1, 0};
+            int[] a = { 0, 0, 1, 0, 0, 1, 0};
             Console.WriteLine(jumpingOnClouds(a));
-            Thread.Sleep(20000000);
         }
     }
 }
