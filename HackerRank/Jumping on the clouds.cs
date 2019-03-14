b37508659bdf6ba65571f3dfb6a9638a49b5de35
arturolc <arturolc_97@hotmail.com>
2019-03-14 13:11:08 -0600

    skeleton ready

---
+++ HackerRank/Jumping on the clouds.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace HackerRank
+{
+    class Program
+    {
+        static int jumpingOnClouds(int[] c)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = { 7, 0, 0, 1, 0, 0, 1, 0};
+            Console.WriteLine(jumpingOnClouds(a));
+            Thread.Sleep(20000000);
+        }
+    }
+}
